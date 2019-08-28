5e18123497a568a7e2593559234dd8ea3747f990
Student <example@example.com>
2019-08-27 18:41:43 -0700

    new forms

--- java/jscript.js
+++ java/jscript.js
@@ -1,22 +1,81 @@
-$(document).ready(function() {
-  $("form#language-Form").submit(function(event) {
-    var age = parseInt($("input#age").val());
-    var freetime = $("select#freetime").val();
-    var hunger = $("select#hunger").val();
-    var name = $("select#name").val();
+// Business Logic
+function letsTalk () {
+  this.tasks = [],
+  this.currentId = 0
+};
 
+toDoList.prototype.addTask = function (Task) {
+  task.id = this.assignId();
+  this.tasks.push(Task)
+}
 
-    var quote = "is "
-    if (gender === 'male' && age < 26 && eyeColor === 'Brown') {
-      quote += 'Sting';
+toDoList.prototype.assignID = function() {
+  this.currentId +=1;
+  return this.currentId
+}
+function Task(taskName, date, priorityLevel, location) {
+  this.taskName = taskName;
+  this.date = date;
+  this.priorityLevel = priorityLevel;
+  this.location = location;
+}
+
+Task.prototype.deleteTask = function(id) {
+  for (var i=0; i< this.tasks.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        delete this.contacts[i];
+        return true
+      }
     }
-    if (gender === 'female' && age > 26 && eyeColor === 'Green') {
-      quote += 'Brittany Spears';
-    }
+  };
+  return false;
+}
 
-    $("#rate").text(quote);
-    $("#quote").show()
 
+
+
+
+
+
+
+
+//User Interface Logic
+
+Task.prototype.display = function () {
+  $("#return-task-name").text(this.taskName);
+  $("#return-task-date").text(this.date);
+  $("#return-task-priority").text(this.priorityLevel);
+  $("#return-task-location").text(this.location);
+};
+
+$(document).ready(function(event) {
+  $("#formOne").submit(function(event) {
     event.preventDefault();
-  });
+    var newTaskName = $("input#task-name").val();
+    var newTaskDate = $("input#task-date").val();
+    var newTaskPriorityLevel = $("#priorityLevel").val();
+    var newTaskLocation = $("input#task-location").val();
+
+    $("#listed-tasks").append(
+      `<div class="${newTaskName}">
+      <ul>
+      <bold><li>Task Name: ${newTaskName}</li></bold>
+      <li>Task Date: ${newTaskDate}</li>
+      <li>Task Priority: ${newTaskPriorityLevel}</li>
+      <li>Task Location: ${newTaskLocation}</li>
+      </ul>
+      <button class="deleteButton">Task Completed!</button>
+      </div>`
+    )
+    $("#story").show();
+
+    $(".deleteButton").click(function() {
+    $(`.${newTaskName}`).hide()
+
+    newTaskName.clear()
+    console.log(`${newTaskName}`)
+  })
+
+  })
 });
