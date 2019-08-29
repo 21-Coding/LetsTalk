08c68c1bbe9d48cc65c8877eee106f2cde0fd8e7
Student <example@example.com>
2019-08-29 08:08:44 -0700

    add new html elements

--- java/jscript.js
+++ java/jscript.js
@@ -1,22 +1,5 @@
 // Business Logic
-function Form(questionOne, questionTwo, questionThree, questionFour) {
-  this.questionOne = questionOne;
-  this.questionTwo = questionTwo;
-  this.questionThree = questionThree;
-  this.questionFour = questionFour;
-}
 
 //User Interface Logic
-var huey = $("#huey")
-var form = document.querySelector("form");
-var log = document.querySelector("#log");
 
-form.addEventListener("submit", function(event) {
-  var data = new FormData(form);
-  var output = "";
-  for (const entry of data) {
-    output = entry[0] + "=" + entry[1] + "\r";
-  };
-  log.innerText = output;
-  event.preventDefault();
-}, false);
+$(document)
