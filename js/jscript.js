c2952237c3c20153baea70d8b8e24063ebc72d46
Dominque <ddyoumans@icloud.com>
2019-09-02 14:02:32 -0400

    new JS and HTML

--- js/jscript.js
+++ js/jscript.js
@@ -1,3 +1,17 @@
-$
-    }
-  });
+$(document).ready(function(){
+  $("form#assess").submit(function(event) {
+    event.preventDefault();
+    var name = $("#name").val();
+    var time = parseInt($("#time").val());
+    var season = parseInt($("#season").val());
+    var holiday = parseInt($("#holiday").val());
+
+    var results = time + season + holiday;
+
+
+    if (results < 2){
+      $("")
+    } )
+
+  })
+})
