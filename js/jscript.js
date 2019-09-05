9faeb13e00f4252af1aed0028b53d97a01646665
nikhilbatta <nbatta08@gmail.com>
2019-09-04 18:12:45 -0700

    more branching

--- js/jscript.js
+++ js/jscript.js
@@ -5,18 +5,20 @@ $(document).ready(function(){
     var time = parseInt($("#time").val());
     var season = parseInt($("#season").val());
     var holiday = parseInt($("#holiday").val());
+    var car = parseInt($("#car").val());
+    var bball = parseInt($("#bball").val());
 
-    var results = time + season + holiday;
+    var results = time + season + holiday + car + bball ;
 
     console.log(results);
-    if (results > 0 && results <= 6 ){
+    if (results >= 8 && results <= 12 ){
       $(".ruby").show();
       $(".JS, .CSharp, .none").hide();
-    } else if (results >= 7 && results <= 12){
+    } else if (results >= 12 && results <= 25){
       $(".JS").show();
       $(".CSharp, .ruby, .none").hide();
 
-    } else if (results > 12){
+    } else if (results >= 25){
       $(".CSharp").show();
       $(".ruby, .none, .JS").hide();
 
